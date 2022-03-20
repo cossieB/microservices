6780e75d8c0dd45b21f74464ec0a64691c10f75a
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-03-20 11:24:53 +0200

    Added timestamp microservice

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import cors from "cors";
 import express from "express";
+import dotenv from 'dotenv';
 import { whoAmIRouter } from "./whoami";
+import { timestampRouter } from "./timestamp";
 
+dotenv.config()
 const app = express()
 
 app.use(cors())
@@ -9,10 +12,11 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile("/public/index.html")
+    res.sendFile("/views/index.html")
 })
 
 app.use('/whoami', whoAmIRouter)
+app.use('/timestamp', timestampRouter)
 
 const PORT = process.env.PORT || 5000;
 
